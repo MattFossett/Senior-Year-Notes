4d759cb5bae4b6fb3673f1a8b2a9f5c7c3d56ed8
Matt Fossett <mattfoss01@gmail.com>
2019-09-14 17:30:44 -0400

    added audio support

--- populateFiles.sh
+++ populateFiles.sh
@@ -3,6 +3,12 @@
 # populateFiles numberedClass date
 
 echo "#### _$2_" >> _includes/$1/$2.md
+if [ $# -eq 3 ]
+    then 
+        echo "<audio controls>" >> _includes/$1/$2.md
+        echo "  <source src=\"/Audio/$1-$2.mp3\" type=\"audio/mpeg\">" >> _includes/$1/$2.md
+        echo "Your browser does not support the audio element.</audio>" >> _includes/$1/$2.md
+fi
 echo "* [$2](#$2)" >> _includes/$1/links.md
 
 echo "" >> $1/index.md
@@ -11,4 +17,3 @@ echo "" >> $1/index.md
 echo "{% include /$1/$2.md %}" >> $1/index.md
 echo "" >> $1/index.md
 echo "*[Back to index](#$1)*" >> $1/index.md
-
